1e0df36363803b022ad0da816d072d0acca1002c
MEGARISSE-Yannick <67727389+MEGARISSE-Yannick@users.noreply.github.com>
2020-10-07 10:57:28 +0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,25 +2,28 @@ var random;
 var signeU = "";
 var signeIA = "";
 var round;
-var scorej=0;
-var scoreIA=0;
+var scorej = 0;
+var scoreIA = 0;
+var score;
+var personj = "";/*nom*/
+var personIA = "";/*nom*/
+
 function modifnom() {
 
     document.getElementById("nom").innerHTML = prompt;
-    var person = prompt("Ton nom?", "Utilisateur");
-    if (person != null) {
-        document.getElementById("nom").innerHTML = person;
+    var personj = prompt("Ton nom?", "Utilisateur");
+    if (personj != null) {
+        document.getElementById("nom").innerHTML = personj;
     }
 } /*prompt pour pouvoir entrer le nom de l'utilisateur*/
 function modifnomIA() {
 
     document.getElementById("nomIA").innerHTML = prompt;
-    var person = prompt("Son nom?", "Ordinateur");
-    if (person != null) {
-        document.getElementById("nomIA").innerHTML = person;
+    var personIA = prompt("Son nom?", "Ordinateur");
+    if (personIA != null) {
+        document.getElementById("nomIA").innerHTML = personIA;
     }
 } /*prompt pour pouvoir entrer le nom de l'IA*/
-
 function modifIA() {
     random = Math.round(Math.random() * 3);
     if (random == 1) {
@@ -63,39 +66,27 @@ function modifimg(signe) {
     }
 
 }/*recupere le choix de l'utilisateur*/
-function ajout2() {
-    var round = document.getElementById("increm2").innerHTML;
-    round++;
-    document.getElementById("increm2").innerHTML = round;
-    document.getElementById("")
-
-}/*incrementation du round */
-function ajout3() {
-    var round = document.getElementById("increm3").innerHTML;
-    round++;
-    document.getElementById("increm3").innerHTML = round;
-    document.getElementById("")
-
-}/*incrementation du round *//*trois le meme code car le glitch ne faisait pas correctement sans */
-
 function ajout1() {
     var round = document.getElementById("increm1").innerHTML;
     round++;
     document.getElementById("increm1").innerHTML = round;
-    document.getElementById("")
+    if (round>=3) {
+        document.getElementById("increm1").innerHTML = "";
+
+    }
     if (round == 1) {
         console.log('tour1')
         if (signeU == 'pierre' && signeIA == 'ciseaux' || signeU == 'ciseaux' && signeIA == 'feuille' || signeU == 'feuille' && signeIA == 'pierre') {
             console.log('joueur win')
             scorej++;
-            console.log(scorej,+' '+ scoreIA)
+            console.log(scorej, +' ' + scoreIA)
 
-            document.getElementById("points-J-1").style.background = 'green';
-            document.getElementById("points-IA-1").style.background = 'red';
+            document.getElementById("points-J-1").style.background = 'rgb(0, 200, 243)';
+            document.getElementById("points-IA-1").style.background = 'rgb(243, 0, 200)';
         }
         else if (signeU == signeIA) {
             console.log('egalite')
-            console.log(scorej,+' '+ scoreIA)
+            console.log(scorej, +' ' + scoreIA)
 
             document.getElementById("points-J-1").style.background = 'gray';
             document.getElementById("points-IA-1").style.background = 'gray';
@@ -103,10 +94,10 @@ function ajout1() {
         else {
             console.log('joueur lose')
             scoreIA++;
-            console.log(scorej,+''+ scoreIA)
+            console.log(scorej, +'' + scoreIA)
 
-            document.getElementById("points-J-1").style.background = 'red';
-            document.getElementById("points-IA-1").style.background = 'green';
+            document.getElementById("points-J-1").style.background = 'rgb(243, 0, 200)';
+            document.getElementById("points-IA-1").style.background = 'rgb(0, 200, 243)';
 
         }
 
@@ -116,14 +107,14 @@ function ajout1() {
         if (signeU == 'pierre' && signeIA == 'ciseaux' || signeU == 'ciseaux' && signeIA == 'feuille' || signeU == 'feuille' && signeIA == 'pierre') {
             console.log('joueur win')
             scorej++;
-            console.log(scorej,+' '+ scoreIA)
+            console.log(scorej, +' ' + scoreIA)
 
-            document.getElementById("points-J-2").style.background = 'green';
-            document.getElementById("points-IA-2").style.background = 'red';
+            document.getElementById("points-J-2").style.background = 'rgb(0, 200, 243)';
+            document.getElementById("points-IA-2").style.background = 'rgb(243, 0, 200)';
         }
         else if (signeU == signeIA) {
             console.log('egalite')
-            console.log(scorej,+' '+ scoreIA)
+            console.log(scorej, +' ' + scoreIA)
 
             document.getElementById("points-J-2").style.background = 'gray';
             document.getElementById("points-IA-2").style.background = 'gray';
@@ -131,10 +122,10 @@ function ajout1() {
         else {
             console.log('joueur lose')
             scoreIA++;
-            console.log(scorej,+' '+ scoreIA)
+            console.log(scorej, +' ' + scoreIA)
 
-            document.getElementById("points-J-2").style.background = 'red';
-            document.getElementById("points-IA-2").style.background = 'green';
+            document.getElementById("points-J-2").style.background = 'rgb(243, 0, 200)';
+            document.getElementById("points-IA-2").style.background = 'rgb(0, 200, 243)';
         }
 
     }/*tour 2 couleur led egalite lose ou win calcule du score */
@@ -143,39 +134,42 @@ function ajout1() {
         if (signeU == 'pierre' && signeIA == 'ciseaux' || signeU == 'ciseaux' && signeIA == 'feuille' || signeU == 'feuille' && signeIA == 'pierre') {
             console.log('joueur win')
             scorej++;
-            console.log(scorej,+' '+ scoreIA)
-
-
-            document.getElementById("points-J-3").style.background = 'green';
-            document.getElementById("points-IA-3").style.background = 'red';
+            console.log(scorej, +' ' + scoreIA)
+            document.getElementById("points-J-3").style.background = 'rgb(0, 200, 243)';
+            document.getElementById("points-IA-3").style.background = 'rgb(243, 0, 200)';
         }
         else if (signeU == signeIA) {
             console.log('egalite')
-            console.log(scorej,+' '+ scoreIA)
-
+            console.log(scorej, +' ' + scoreIA)
             document.getElementById("points-J-3").style.background = 'gray';
             document.getElementById("points-IA-3").style.background = 'gray';
         }
         else {
             console.log('joueur lose')
             scoreIA++;
-            console.log(scorej,+' '+ scoreIA)
+            console.log(scorej, +' ' + scoreIA)
 
-            document.getElementById("points-J-3").style.background = 'red';
-            document.getElementById("points-IA-3").style.background = 'green';
+            document.getElementById("points-J-3").style.background = 'rgb(243, 0, 200)';
+            document.getElementById("points-IA-3").style.background = 'rgb(0, 200, 243)';
         }
 
     }/*tour 3 couleur led egalite lose ou win calcule du score */
-}
-function score() {
-    if (scorej>scoreIA) {
-        console.log('gg tu gagne')
-    }
-    else if (scoreIA>scorej) {
-        console.log('pas gg tu perd')
-    }
-    else{
-        console.log('match nul parfait')
-    }
-}
+    while (round >= 3) {
+        if (scorej > scoreIA) {
+            console.log('gg tu gagne')
+            document.getElementById("winner").innerHTML = scorej + '-' + scoreIA + " " + "Gagné";
 
+        }
+        else if (scoreIA > scorej) {
+            console.log('pas gg tu perd')
+            document.getElementById("winner").innerHTML = scorej + '-' + scoreIA + " " + "Perdu";
+
+        }
+        else {
+            console.log('match nul parfait')
+            document.getElementById("winner").innerHTML = scorej + "-" + scoreIA + " " + "Egalité";
+
+        }
+        break;
+    }
+}
